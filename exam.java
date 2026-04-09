19f3712b59231854132c67299315e9b959b714b6
곽영빈 <25_04@bssm.hs.kr>
2026-04-09 15:35:00 +0900

    add: 2301 곽영빈

--- exam.java
+++ exam.java
@@ -1,1 +1,28 @@
+class Potion {
 
+}
+
+class Weapon {
+
+}
+
+class Monster {
+    void kill() {
+
+    }
+}
+
+class Character {
+    Potion potion;
+    Weapon weapon;
+
+    public Character() { }
+
+    void getWeapon(Weapon weapon) {
+        this.weapon = weapon;
+    }
+
+    void kill(Monster monster) {
+        monster.kill();
+    }
+}
